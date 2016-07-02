f6d737b8849cd4ade8dc1dcb0d6d79fec7ea67dc
Lefort Pablo <lefortpablo@gmail.com>
2016-07-02 17:14:15 -0300

    WIP Scan Qr code

--- app/src/main/java/com/example/pablo/smartwallet/MainActivity.java
+++ app/src/main/java/com/example/pablo/smartwallet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pablo.smartwallet;
 
+import android.app.AlertDialog;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -7,37 +8,114 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
-    //private Uri fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+    private ZXingScannerView mScannerView;
+    Integer response = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        final MainActivity self = this;
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                //startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                //QrScanner(view);
+
+                mScannerView = new ZXingScannerView(self);
+                setContentView(mScannerView);
+                mScannerView.setResultHandler(self);
+                mScannerView.startCamera();
             }
         });
+    }
 
-        intent.
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if(response == 1) {
+            mScannerView = null;
+            mScannerView = new ZXingScannerView(this);
+            setContentView(mScannerView);
+            response = 0;
+
+        }
+        if(mScannerView != null){
+            mScannerView.setResultHandler(this);
+            mScannerView.startCamera();
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //mScannerView.stopCamera();   // Stop camera on pause
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        /*if(mScannerView != null){
+            mScannerView.stopCamera();
+            mScannerView = null;
+        }*/
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        mScannerView.stopCameraPreview();
+        mScannerView.stopCamera();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Toast.makeText(this, "Image saved to:\n" +
+                        data.getData(), Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                // Image capture failed, advise user
+            }
+        }
     }
 
     @Override
