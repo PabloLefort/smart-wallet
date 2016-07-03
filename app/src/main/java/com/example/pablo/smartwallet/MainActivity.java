4232d2c8f22804ff3642f37701e77dbe1d5a5556
Lefort Pablo <lefortpablo@gmail.com>
2016-07-03 15:43:58 -0300

    V1

--- app/src/main/java/com/example/pablo/smartwallet/MainActivity.java
+++ app/src/main/java/com/example/pablo/smartwallet/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.pablo.smartwallet;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,36 +20,51 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int SCAN_QR_ACTIVITY_REQUEST_CODE = 200;
-    private static String[][] datos = new String[0][2];
     private static CardView card;
     private static TextView txtnombre;
     private static TextView txtprecio;
+    private static Button finalizar;
+    private static Bitmap photo;
+    private static String SCAN_FACE_REQUEST_URL = "http://localhost:8000/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_login);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         txtnombre = (TextView) findViewById(R.id.nombre);
         txtprecio = (TextView) findViewById(R.id.precio);
+        finalizar = (Button) findViewById(R.id.finalizar);
         card = (CardView) findViewById(R.id.cv);
+
         card.setVisibility(View.GONE);
+        finalizar.setVisibility(View.GONE);
 
-        //RecyclerView recycler = (RecyclerView) findViewById(R.id.recycler);
-
-        //recycler.setHasFixedSize(true);
-        //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
     }
 
@@ -56,13 +75,51 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(scanIntent, SCAN_QR_ACTIVITY_REQUEST_CODE);
     }
 
+    public void finishBuy(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //intent.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        //intent.putExtra(MediaStore.EXTRA_OUTPUT, AppConstant.URI_IMAGE_CAPTURED);
+        //intent.putExtra("android.intent.extras.CAMERA_FACING", android.hardware.camera2.);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        /*try {
+            //create a file to write bitmap data
+            //File f = new File(context.getCacheDir(), filename);
+            File f = new File("testing.png");
+            f.createNewFile();
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.PNG, 0, bos);
+            byte[] bitmapdata = bos.toByteArray();
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+
+            MultipartUtility multipart = new MultipartUtility(SCAN_FACE_REQUEST_URL, "utf-8");
+            multipart.addFilePart("fileee", f);
+            List<String> response = multipart.finish();
+            System.out.println("SERVER REPLIED:");
+            for (String line : response) {
+                System.out.println("Upload Files Response:::" + line);
+                System.out.println(line);
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, "Error", Toast.LENGTH_LONG).show();
+        }*/
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+                //Toast.makeText(this, "Saved to:\n" +data.getData(), Toast.LENGTH_LONG).show();
+                //photo = (Bitmap) data.getExtras().get("data");
+                System.out.println("PHOTO: "+photo);
+                setContentView(R.layout.success);
+
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
             } else {
@@ -73,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == SCAN_QR_ACTIVITY_REQUEST_CODE){
             if(resultCode == RESULT_OK){
                 card.setVisibility(View.VISIBLE);
+                finalizar.setVisibility(View.VISIBLE);
                 txtnombre.setText("Prenda: "+data.getStringExtra("nombre"));
                 txtprecio.setText("Precio: "+data.getStringExtra("precio"));
             }
